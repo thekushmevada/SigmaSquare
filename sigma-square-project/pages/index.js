43467741ca9befbedb2ae55e9b31093b9cf47fac
vatsal38 <101249643+vatsal38@users.noreply.github.com>
2022-12-18 17:16:10 +0530

    ok

--- sigma-square-project/pages/index.js
+++ sigma-square-project/pages/index.js
@@ -11,7 +11,7 @@ const Home = () => {
       <div className="-mt-10">
         <AboutPage />
       </div>
-      <h1 className="text-9xl">Team Section Dishatnnnnn </h1>
+      <h1 className="text-9xl">Team Section </h1>
       <Footer />
     </>
   );
