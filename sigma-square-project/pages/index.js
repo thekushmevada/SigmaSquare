e2dfca01bcb9f685df9e3ce394a541b70365015c
vatsal38 <vatsal9999.vm@gmail.com>
2022-12-18 11:55:41 +0530

    Update index.js

--- sigma-square-project/pages/index.js
+++ sigma-square-project/pages/index.js
@@ -3,16 +3,18 @@ import HeaderMain from "../components/Header/HeaderMain";
 import Footer from "../components/footer/Footer";
 import ImgPagination from "../components/HomePage/ImgPagination";
 import AboutPage from "../components/aboutPage/AboutPage";
+import Login from "../components/loginpage/Login";
 const Home = () => {
   return (
     <>
-      <HeaderMain />
+      {/* <HeaderMain />
       <ImgPagination />
       <div className="-mt-10">
         <AboutPage />
       </div>
       <h1 className="text-9xl">Team Section </h1>
-      <Footer />
+      <Footer /> */}
+      <Login/>
     </>
   );
 };
