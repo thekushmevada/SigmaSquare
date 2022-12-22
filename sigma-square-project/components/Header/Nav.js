dcd328067ae80b6957f76f9ebad3632acc61110c
vatsal38 <vatsal9999.vm@gmail.com>
2022-12-22 12:10:52 +0530

    login change

--- sigma-square-project/components/Header/Nav.js
+++ sigma-square-project/components/Header/Nav.js
@@ -1,20 +1,41 @@
 import Link from "next/link";
+import Login from "../loginpage/Login";
+import Join from "../joinPage/Join";
+import { useContext } from "react";
+import { layoutStateContext } from "../../context/LayoutContext";
 const Nav = () => {
+  const {
+    showLogin,
+    handleCloseLogin,
+    handleShowLogin,
+    showJoin,
+    handleCloseJoin,
+    handleShowJoin,
+  } = useContext(layoutStateContext);
   return (
     <nav className="bg-[#4E4A3F]">
       <div className="container mx-auto px-4 lg:px-24 md:px-12 sm:w-6 flex items-center justify-between font-sans h-24 ">
         <div className="text-[#CABA93] font-bold text-4xl">Sigma Square</div>
         <div className="flex gap-2">
-          <Link href="/login">
-            <button className="bg-[#6F6657] hover:bg-[#4E4A3F] text-[#BBB093] font-bold py-2 rounded-full w-full md:w-40 sm:w-20 px-3 sm:px-2">
-              Login
-            </button>
-          </Link>
-          <Link href="/join">
-            <button className="bg-[#6F6657] hover:bg-[#4E4A3F] text-[#BBB093] font-bold py-2 rounded-full w-full md:w-40 sm:w-20 px-3 sm:px-2">
-              Join
-            </button>
-          </Link>
+          <button
+            className={`${
+              showLogin ? "hidden" : "block"
+            } bg-[#6F6657] hover:bg-[#4E4A3F] text-[#BBB093] font-bold py-2 rounded-full w-full md:w-40 sm:w-20 px-3 sm:px-2`}
+            onClick={handleShowLogin}
+          >
+            Login
+          </button>
+          <Login showLogin={showLogin} handleCloseLogin={handleCloseLogin} />
+
+          <button
+            className={`${
+              showJoin ? "hidden" : "block"
+            } bg-[#6F6657] hover:bg-[#4E4A3F] text-[#BBB093] font-bold py-2 rounded-full w-full md:w-40 sm:w-20 px-3 sm:px-2`}
+            onClick={handleShowJoin}
+          >
+            Join
+          </button>
+          <Join showJoin={showJoin} handleCloseJoin={handleCloseJoin} />
         </div>
       </div>
     </nav>
--- sigma-square-project/components/joinPage/Join.js
+++ sigma-square-project/components/joinPage/Join.js
@@ -1,25 +1,20 @@
-import Link from "next/link";
-import React from "react";
-
-const Join = () => {
+import Modal from "react-bootstrap/Modal";
+const Join = (props) => {
+  const { showJoin, handleCloseJoin } = props;
   return (
     <>
-      <section className="bg-gray-50 dark:bg-gray-900">
-        <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-          <div className="w-full bg-white rounded-lg shadow  md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
-            <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-              <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-                Create and account
-              </h1>
-              <form
-                className="space-y-4 md:space-y-6"
-                action="/api/register"
-                method="post"
-              >
+      <Modal show={showJoin} onHide={handleCloseJoin}>
+        <div className="bg-[#2e2b22] ">
+          <h1 className="text-center text-4xl font-sans text-[#CABA93] pt-7 font-semibold">
+            Join
+          </h1>
+          <section>
+            <div className="flex flex-col items-center justify-center px-2 py-8 w-full gap-4">
+              <form className="space-y-4 w-full px-4" action="#">
                 <div>
                   <label
                     for="email"
-                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                    className="block mb-2 text-sm font-medium text-white"
                   >
                     Your email
                   </label>
@@ -27,7 +22,7 @@ const Join = () => {
                     type="email"
                     name="email"
                     id="email"
-                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    className="bg-[#D7CFC1] border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 "
                     placeholder="name@company.com"
                     required=""
                   />
@@ -35,7 +30,7 @@ const Join = () => {
                 <div>
                   <label
                     for="password"
-                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                    className="block mb-2 text-sm font-medium text-white"
                   >
                     Password
                   </label>
@@ -44,66 +39,47 @@ const Join = () => {
                     name="password"
                     id="password"
                     placeholder="••••••••"
-                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    className="bg-[#D7CFC1] border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 "
                     required=""
                   />
                 </div>
-                <div>
-                  <label
-                    for="confirm-password"
-                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                  >
-                    Confirm password
-                  </label>
-                  <input
-                    type="confirm-password"
-                    name="confirm-password"
-                    id="confirm-password"
-                    placeholder="••••••••"
-                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                    required=""
-                  />
-                </div>
-                <div className="flex items-start">
-                  <div className="flex items-center h-5">
-                    <input
-                      id="terms"
-                      aria-describedby="terms"
-                      type="checkbox"
-                      className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
-                      required=""
-                    />
+                <div className="flex items-center justify-between">
+                  <div className="flex items-start">
+                    <div className="flex items-center h-5">
+                      <input
+                        id="remember"
+                        aria-describedby="remember"
+                        type="checkbox"
+                        className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300"
+                        required=""
+                      />
+                    </div>
+                    <div className="ml-3 text-sm">
+                      <label for="remember" className="text-white">
+                        Remember me
+                      </label>
+                    </div>
                   </div>
-                  <div className="ml-3 text-sm">
-                    <label
-                      for="terms"
-                      className="font-light text-gray-500 dark:text-gray-300"
-                    >
-                      I accept the{" "}
-                      <a
-                        className="font-medium text-primary-600 hover:underline dark:text-primary-500"
-                        href="#"
-                      >
-                        Terms and Conditions
-                      </a>
-                    </label>
-                  </div>
+                  <span className="text-sm font-medium text-white hover:underline ">
+                    Forgot password?
+                  </span>
                 </div>
-                <button
-                  type="submit"
-                  className="w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
-                >
-                  Create an account
+                <button className="bg-[#6F6657] w-44 h-12 rounded-lg">
+                  <span className="font-sans font-medium text-xl text-[#FEFCF9]">
+                    Log In
+                  </span>
                 </button>
-                <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-                  Already have an account?{" "}
-                  <Link href="/login">Login here.</Link>
+                <p className="text-sm font-light text-white ">
+                  Don’t have an account yet?{" "}
+                  <span className="font-medium text-white hover:underline ">
+                    Sign up
+                  </span>
                 </p>
               </form>
             </div>
-          </div>
+          </section>
         </div>
-      </section>
+      </Modal>
     </>
   );
 };
