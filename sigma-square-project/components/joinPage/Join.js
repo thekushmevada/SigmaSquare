52c4348a5878624499c51c497cd202c683a3f3a8
thekushmevada <kush_20092@ldrp.ac.in>
2023-01-10 18:34:45 +0530

    register semi final

--- sigma-square-project/components/joinPage/Join.js
+++ sigma-square-project/components/joinPage/Join.js
@@ -105,6 +105,19 @@ const Join = (props) => {
 
                 </div>
               </form>
+
+              <form action="/api/register" method="post">
+        <label>
+          EMail Address
+        </label>
+        <input type='email' name='email' placeholder='Type your email'></input>
+        <label>
+          Pasword
+        </label>
+        <input type='password' name='password' placeholder='Type your password'></input>
+        <input type='submit' value='Register'></input>
+
+      </form>
             </div>
           </section>
         </div>
