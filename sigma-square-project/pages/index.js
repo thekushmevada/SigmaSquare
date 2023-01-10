ea3cfdbf7abc306f81f0a7be8806edf408d385b8
thekushmevada <kush_20092@ldrp.ac.in>
2023-01-10 19:10:09 +0530

    Format Document

--- sigma-square-project/pages/index.js
+++ sigma-square-project/pages/index.js
@@ -4,47 +4,18 @@ import Footer from "../components/footer/Footer";
 import ImgPagination from "../components/HomePage/ImgPagination";
 import AboutPage from "../components/aboutPage/AboutPage";
 
-export default function Home()  {
+export default function Home() {
   return (
     <>
       <HeaderMain />
       <ImgPagination />
 
-      
       <AboutPage />
-      <div>
-      <h1> Registraion </h1>
-      <form action="/api/register" method="post">
-        <label>
-          EMail Address
-        </label>
-        <input type='email' name='email' placeholder='Type your email'></input>
-        <label>
-          Pasword
-        </label>
-        <input type='password' name='password' placeholder='Type your password'></input>
-        <input type='submit' value='Register'></input>
-
-      </form>
-
-      <h1>Login</h1>
-      <form action="/api/login" method="post">
-        <label>
-          EMail Address
-        </label>
-        <input type='email' name='email' placeholder='Type your email'></input>
-        <label>
-          Pasword
-        </label>
-        <input type='password' name='password' placeholder='Type your password'></input>
-        <input type='submit' value='Login'></input>
-
-      </form>
-    </div>
+      <h1>
+        <center>Kush was Here</center>
+      </h1>
 
       <Footer />
     </>
   );
-};
-
-
+}
