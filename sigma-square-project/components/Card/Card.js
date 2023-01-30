ff4abe4c0eeeacf1b0c3c1faba2bad1fb0487e34
vatsal38 <vatsal9999.vm@gmail.com>
2023-01-30 20:54:03 +0530

    set the image

--- sigma-square-project/components/Card/Card.js
+++ sigma-square-project/components/Card/Card.js
@@ -2,26 +2,33 @@ import React from "react";
 import Sigma_Logo_2 from "../../img/Sigma_Logo_2.png";
 import Image from "next/image";
 import { MdDescription } from "react-icons/md";
-
+import vatsal from "../../img/vatsal.jpg";
+import kush from "../../img/kush.jpg";
+import maitri from "../../img/maitri.jpg";
+import dishant from "../../img/dishant.jpg";
 const Card = () => {
   const partnersData = [
     {
       name: "Vatsal Mavani",
+      src: vatsal,
       description:
         "Lorem ipsum dolor sit amet, consectetur adipis elit Voluptatibus",
     },
     {
       name: "Kush Mevada",
+      src: kush,
       description:
         "Lorem ipsum dolor sit amet, consectetur adipis elit Voluptatibus",
     },
     {
       name: "Maitri Parmar",
+      src: maitri,
       description:
         "Lorem ipsum dolor sit amet, consectetur adipis elit Voluptatibus",
     },
     {
       name: "Dishant Korat",
+      src: dishant,
       description:
         "Lorem ipsum dolor sit amet, consectetur adipis elit Voluptatibus",
     },
@@ -38,12 +45,14 @@ const Card = () => {
             return (
               <div className="flex flex-col items-center max-w-md rounded-3xl overflow-hidden shadow-md p-6 bg-[#134F59]">
                 <Image
-                  src={Sigma_Logo_2}
+                  src={data.src}
                   alt="Partners"
-                  style={{ height: "auto", maxWidth: "250px" }}
+                  style={{ height: "auto", maxWidth: "250px",borderRadius:"100%" }}
                 />
                 <div className="px-6 py-1 flex flex-col items-center">
-                  <div className="font-bold text-xl mb-2 text-[#CABA93]">{data.name}</div>
+                  <div className="font-bold text-xl mb-2 text-[#CABA93]">
+                    {data.name}
+                  </div>
                   <p className="text-white text-base">{data.description}</p>
                 </div>
               </div>
